d481c5adf8957b960eca3d4bc7a56510952151da
miodrag <mpavkovic@gmail.com>
2016-03-20 19:34:09 +0100

    select customer

--- src/server/server.ts
+++ src/server/server.ts
@@ -9,17 +9,14 @@ app.use('/assets', express.static(path.resolve(__dirname, 'assets')));
 app.use('/styles', express.static(path.resolve(__dirname, 'styles')));
 app.use('/fonts', express.static(path.resolve(__dirname, 'assets/fonts')));
 
-var renderIndex = (req: express.Request, res: express.Response) => {
-    console.log("renderIndex __dirname", __dirname)
-    res.sendFile(path.resolve(__dirname, 'index.html'));
-}
+
 var renderCustomer = (req: express.Request, res: express.Response) => {
     console.log("renderCustomer")
     res.sendFile(path.resolve(__dirname, 'customers.json'));
 }
 app.get('/getcustomers', renderCustomer);
-app.get('/*', renderIndex);
-app.get('/selectCustomers', selectCustomers);
+
+
 
 
 var server = app.listen(port, function() {
@@ -32,7 +29,7 @@ var mysql = require('mysql'); // node-mysql module
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
-  password : '',
+  password : 'root',
   database : 'smiletime'
 });
 
@@ -47,11 +44,16 @@ var selectCustomers = (req: express.Request, res: express.Response) => {
     connection.query("SELECT * from patient",function(err,rows){
     if(err) {
         console.log("Problem with MySQL"+err);
-        res.sendFile(path.resolve(__dirname, 'customers.json'));
-      } else {
-        res.json({"Error" : false, "Message" : "Success", "Customers" : rows});
-          //res.end(JSON.stringify(rows));
-      }
+        res.send(path.resolve(__dirname, 'customers.json'));
+    } else {
+     console.log('The solution is: ', rows[0].solution);        
+        res.send(JSON.stringify(rows));
+    }
   });
 }
-
+app.get('/selectCustomers', selectCustomers);
+var renderIndex = (req: express.Request, res: express.Response) => {
+    console.log("renderIndex __dirname", __dirname)
+    res.sendFile(path.resolve(__dirname, 'index.html'));
+}
+app.get('/*', renderIndex);

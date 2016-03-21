43880a0404bf85eaff496c52dd77a4e743092583
miodrag <mpavkovic@gmail.com>
2016-03-21 21:11:27 +0100

    server,ts console log.

--- src/server/server.ts
+++ src/server/server.ts
@@ -46,14 +46,29 @@ var selectCustomers = (req: express.Request, res: express.Response) => {
         console.log("Problem with MySQL"+err);
         res.send(path.resolve(__dirname, 'customers.json'));
     } else {
-     console.log('The solution is: ', rows[0].solution);        
+     console.log('The solution is: ', rows[0]);        
         res.send(JSON.stringify(rows));
     }
   });
 }
 app.get('/selectCustomers', selectCustomers);
+
+var selectTreatments = (req: express.Request, res: express.Response) => {
+    console.log("selectTreatments")
+    connection.query("SELECT * from treatment",function(err,rows){
+    if(err) {
+        console.log("Problem with MySQL"+err);
+        res.send(path.resolve(__dirname, 'orders.json'));
+    } else {
+     console.log('The solution is: ', rows[0]);        
+        res.send(JSON.stringify(rows));
+    }
+  });
+}
+app.get('/selectTreatments', selectTreatments);
 var renderIndex = (req: express.Request, res: express.Response) => {
     console.log("renderIndex __dirname", __dirname)
     res.sendFile(path.resolve(__dirname, 'index.html'));
 }
 app.get('/*', renderIndex);
+//connection.end();
